d71851f9327709fb0cc19cfb1c12b28d0e29c44e
Nasuha Asri <NASAS@orsted.com>
2023-11-01 15:38:10 +0800

    Module 3: Setup the program.cs file

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -9,6 +9,23 @@ builder.Services.AddControllersWithViews(); // ensure the app knows about ASP.NE
 var app = builder.Build();
 
 // MapGet is a middleware components that listen incoming requests to the root application
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+// add middleware components
+// bring in support for returning static files - to look for incoming request for static files (JPEG/CSS)
+// it will find it in the wwwroot folder and return it
+// it will then short circuit the request
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    // see errors inside the executing application (a diagnostic middleware component)
+    app.UseDeveloperExceptionPage();
+
+    // to know our environment, go to properties (right click on the project) > debug > general
+}
+
+// ability to be able to navigate to view (ensure ASP.NET Core able to handle incoming requests correctly)
+app.MapDefaultControllerRoute();
 
 app.Run();
