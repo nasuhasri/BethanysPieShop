6a80b75586428cd2e383c65e76337fe5502fe124
Nasuha Asri <NASAS@orsted.com>
2023-11-02 11:24:34 +0800

    - create model (Pie.cs and Category.cs) - create IPieRepository and MockPieRepository as an interface - create MockPieRepository and MockCategoryRepository to implement the concrete class of the interface class

---
+++ BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace BethanysPieShop.Models;
+
+// going to be used to wrap all the data interaction logic
+// make sure consuming classes only talk to the repository
+public interface IPieRepository
+{
+    IEnumerable<Pie> AllPies { get; }
+    IEnumerable<Pie> PiesOfTheWeek { get; }
+    Pie? GetPieById(int pieId);
+}
---
+++ BethanysPieShop/Models/Pie.cs
@@ -0,0 +1,21 @@
+﻿namespace BethanysPieShop.Models;
+
+public class Pie
+{
+    public int PieId { get; set; }
+    // set name as empty
+    public string Name { get; set; } = string.Empty;
+    // ? = nullable = do not require a value = can be null in database
+    public string? ShortDescription { get; set; }
+    public string? LongDescription { get; set; }
+    public string? AllergyInformation { get; set; }
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? ImageThumbnailUrl { get; set; }
+    public bool IsPieOfTheWeek { get; set; }
+    public bool InStock { get; set; }
+    public int CategoryId { get; set; }
+    // default! - null forgiving operator
+    // used in combination with nullable to declare that category shouldnt be null
+    public Category Category { get; set; } = default!;
+}
