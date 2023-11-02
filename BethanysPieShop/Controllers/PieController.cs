e2c39a2b3bcfd7b3825fc96a3e88d8b8afceec45
Nasuha Asri <NASAS@orsted.com>
2023-11-02 16:39:04 +0800

    added view page

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -24,6 +24,11 @@ public class PieController : Controller
 
     public IActionResult List()
     {
+        // ViewBag is dynamic - can add whatever property we want (shareable between views and controllers)
+        // view able to access the properties inside the ViewBag
+        ViewBag.CurrentCategory = "Cheese cakes";
+
+        // the view is being passed as an IEnumerable in pies
         return View(_pieRepository.AllPies);
     }
 }
