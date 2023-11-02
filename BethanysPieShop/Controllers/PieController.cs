235e5d32b178e0c132ce167ba485daff2866f84c
Nasuha Asri <NASAS@orsted.com>
2023-11-02 16:17:16 +0800

    added controller

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,29 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers;
+
+public class PieController : Controller
+{
+    //public IActionResult Index()
+    //{
+    //    return View();
+    //}
+
+    // local fields
+    private readonly IPieRepository _pieRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    // using constructor injection (the Mock one will be injected)
+    // got the concrete instances after the constructor has run
+    public PieController (IPieRepository pieRepository, ICategoryRepository categoryRepository)
+    { 
+        _pieRepository = pieRepository;
+        _categoryRepository = categoryRepository;
+    }
+
+    public IActionResult List()
+    {
+        return View(_pieRepository.AllPies);
+    }
+}
