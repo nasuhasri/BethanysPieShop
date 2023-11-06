a9bf14102315fc35f3c7a1f6c789d0da2e107d51
Nasuha Asri <NASAS@orsted.com>
2023-11-06 12:05:48 +0800

    add ef core and sql server to use db context

---
+++ BethanysPieShop/Models/BethanysPieShopdbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models;
+
+public class BethanysPieShopDbContext : DbContext
+{
+    //add constructor - this is required
+    public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options)
+    {
+
+    }
+
+    // exposed the entities, pie and category inside of the DBContext as a DbSet
+    // behind the scene, they will be mapped to db tables
+    // EF Core ensure entities get loaded from the db and we will be able to save it if necessary
+
+    // set up the DbSet property
+    // we've basically set to EF Core whereby these entities we want to match
+    // 1) able to load data in these DbSets, 2) make changes to the data, 3) let the updated data flow back to the db when we want to save
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Pie> Pies { get; set; }
+}
--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,12 +1,10 @@
 // execute as console application
 
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 // CreateBuilder will ensure Kestrel is included and set up IIS integration
 var builder = WebApplication.CreateBuilder(args);
-
-// add a service - we bring in framework services that enable MVC in our app
-builder.Services.AddControllersWithViews(); // ensure the app knows about ASP.NET Core MVC
 
 /*
  * when using repositories, use AddScoped -> will create a singleton while request is being handled.
@@ -19,10 +17,25 @@ builder.Services.AddControllersWithViews(); // ensure the app knows about ASP.NE
 builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 
+// add a service - we bring in framework services that enable MVC in our app
+builder.Services.AddControllersWithViews(); // ensure the app knows about ASP.NET Core MVC
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
+
 var app = builder.Build();
 
 // MapGet is a middleware components that listen incoming requests to the root application
 //app.MapGet("/", () => "Hello World!");
+
+// add middleware components
+// bring in support for returning static files - to look for incoming request for static files (JPEG/CSS)
+// it will find it in the wwwroot folder and return it
+// it will then short circuit the request
+app.UseStaticFiles();
 
 if (app.Environment.IsDevelopment())
 {
@@ -31,12 +44,6 @@ if (app.Environment.IsDevelopment())
 
     // to know our environment, go to properties (right click on the project) > debug > general
 }
-
-// add middleware components
-// bring in support for returning static files - to look for incoming request for static files (JPEG/CSS)
-// it will find it in the wwwroot folder and return it
-// it will then short circuit the request
-app.UseStaticFiles();
 
 // ability to be able to navigate to view (ensure ASP.NET Core able to handle incoming requests correctly)
 app.MapDefaultControllerRoute();
