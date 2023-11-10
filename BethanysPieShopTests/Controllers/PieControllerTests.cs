2855a4e36a293b7d5441fa157141a193bfa986e5
Nasuha Asri <NASAS@orsted.com>
2023-11-10 23:25:01 +0800

    create unit tests for controller

---
+++ BethanysPieShopTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using BethanysPieShop.Controllers;
+using BethanysPieShop.ViewModels;
+using BethanysPieShopTests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BethanysPieShopTests.Controllers;
+
+public class PieControllerTests
+{
+    [Fact] // using this attribute, this becomes unit testing method
+    public void List_EmptyCategory_ReturnsAllPies()
+    {
+        // arrange
+        var mockPieRepository = RepositoryMocks.GetPieRepository();
+        var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+
+        var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+        // act
+        var result = pieController.List("");
+
+        //PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "All Pies");
+        
+        // assert
+        // check return type of ViewResult - answer question: did we controller return the ViewResult?
+        var viewResult = Assert.IsType<ViewResult>(result);
+        // check if this one (viewResult.ViewData.Model) is the model we passed in -> PieListViewModel
+        var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+        Assert.Equal(10, pieListViewModel.Pies.Count());
+    }
+}
