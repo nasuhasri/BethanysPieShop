bfd0b5cdfde1e49550cbbd55a3f8f09a283ddc61
Nasuha Asri <NASAS@orsted.com>
2023-11-10 15:25:22 +0800

    - add checkout method in OrderController - add checkout view after user submit the order

--- BethanysPieShop/Controllers/OrderController.cs
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -14,8 +14,45 @@ public class OrderController : Controller
         _shoppingCart = shoppingCart;
     }
 
+    // if we dont specify anything, GET request assumed
     public IActionResult Checkout()
     {
+        return View();
+    }
+
+    [HttpPost] // POST method
+    public IActionResult Checkout(Order order)
+    {
+        // check if there is items in their shopping cart
+        var items = _shoppingCart.GetShoppingCartItems();
+        _shoppingCart.ShoppingCartItems = items;
+
+        /*
+         * ModelState is like a side product of model binding. When model binding is happening, any errors occurred will appear in this ModelState
+         */
+        if (_shoppingCart.ShoppingCartItems.Count() == 0)
+        {
+            ModelState.AddModelError("", "Your cart is empty, add some pies first.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _orderRepository.CreateOrder(order);
+            _shoppingCart.ClearCart();
+
+            // redirect the user to another view
+            // redirect user to another action
+            return RedirectToAction("CheckoutComplete");
+        }
+
+        // if ModelState not valid, it will return the same view which is checkout view and passing in an order that will enable the view to repopulate the fields
+        return View(order);
+    }
+
+    public IActionResult CheckoutComplete()
+    {
+        ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our delicious pies.";
+
         return View();
     }
 }
